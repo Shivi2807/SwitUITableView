2891123dc6560d9a8957692f9ea3c449970c5837
Shivi1 <sagarwal2896@gmail.com>
2022-03-24 12:19:34 +0530

    Initial Commit

---
+++ Shared/SwiftUIVideosTableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVideosTableViewApp.swift
+//  Shared
+//
+//  Created by Shivi Agarwal on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVideosTableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
